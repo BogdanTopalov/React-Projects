3c01869b4c86abf0b5924890e30f32c0aca4eb21
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-06 18:34:53 +0300

    Add Book and BookList components

--- Practice/conditional-rendering-and-state/src/App.js
+++ Practice/conditional-rendering-and-state/src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import { Counter } from './components/Counter';
+import { BookList } from './components/BookList'
 
 
 function App() {
+  const books = [
+    { "title": "Northanger Abbey", "author": "Austen, Jane", "year": 1814, "price": 18.2 },
+    { "title": "War and Peace", "author": "Tolstoy, Leo", "year": 1865, "price": 12.7 },
+    { "title": "Anna Karenina", "author": "Tolstoy, Leo", "year": 1875, "price": 13.5 },
+    { "title": "Mrs. Dalloway", "author": "Woolf, Virginia", "year": 1925, "price": 25 },
+    { "title": "The Hours", "author": "Cunnningham, Michael", "year": 1999, "price": 12.35 },
+    { "title": "Huckleberry Finn", "author": "Twain, Mark", "year": 1865, "price": 5.76 },
+    { "title": "Bleak House", "author": "Dickens, Charles", "year": 1870, "price": 5.75 },
+    { "title": "Tom Sawyer", "author": "Twain, Mark", "year": 1862, "price": 7.75 },
+    { "title": "A Room of One's Own", "author": "Woolf, Virginia", "year": 1922, "price": 29 },
+    { "title": "Harry Potter", "author": "Rowling, J.K.", "year": 2000, "price": 19.95 },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter/>
+        <Counter />
+        <BookList books={books} />
       </header>
     </div>
   );
---
+++ Practice/conditional-rendering-and-state/src/components/BookList.js
@@ -0,0 +1,53 @@
+import { Book } from "./Book"
+
+export const BookList = (props) => {
+    // Example 1
+    // const bookElements = []
+    // for (const book of props.books) {
+    //     bookElements.push(
+    //         <li>
+    //             <article>
+    //                 <h2>{book.title}</h2>
+    //                 <div>Year: {book.year}</div>
+    //                 <div>Price: {book.price}</div>
+    //                 <footer>
+    //                     <span>Author: {book.author}</span>
+    //                 </footer>
+    //             </article>
+    //         </li>
+    //     )
+    // }
+
+    // Example 2
+    // const bookElements = props.books.map(book => (
+    //     <li>
+    //         <article>
+    //             <h2>{book.title}</h2>
+    //             <div>Year: {book.year}</div>
+    //             <div>Price: {book.price}</div>
+    //             <footer>
+    //                 <span>Author: {book.author}</span>
+    //             </footer>
+    //         </article>
+    //     </li>
+    // ))
+
+    // Example 3
+    // const bookElements = props.books.map(book =>
+    //     <Book
+    //         title={book.title}
+    //         year={book.year}
+    //         price={book.price}
+    //         author={book.author}
+    //     />
+    // )
+
+    // Example 4
+    const bookElements = props.books.map(book => <Book {...book}/>)
+
+    return (
+        <ul>
+            {bookElements}
+        </ul>
+    )
+}
