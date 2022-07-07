7ca54ece453cf02c76839499b2b1336ada79b984
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-07 21:08:00 +0300

    Update Book.js

--- Practice/conditional-rendering-and-state/src/components/Book.js
+++ Practice/conditional-rendering-and-state/src/components/Book.js
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react"
+import styles from './Book.module.css'
+
 export const Book = (props) => {
+    const [highlghted, setHighlighted] = useState(false)
+    const [deleted, setDeleted] = useState(false)
+    
+    useEffect(() => {
+        
+    }, [])
+    
+    const clickHandler = () => {
+        setHighlighted(state => !state)
+    }
+
+    const deleteHandler = () => {
+        setDeleted(true)
+    }
+    
+    let style = {}
+    if (highlghted) {
+        style.backgroundColor = 'blue'
+    }
+
+    if (deleted) {
+        return
+    }
+    
     return (
-        <li>
+        <li style={style} className={styles['book']}>
             <article>
                 <h2>{props.title}</h2>
                 <div>Year: {props.year}</div>
@@ -8,6 +35,8 @@ export const Book = (props) => {
                 <footer>
                     <span>Author: {props.author}</span>
                 </footer>
+                <button onClick={clickHandler}>Highlight</button>
+                <button onClick={deleteHandler}>Delete</button>
             </article>
         </li>
     )
