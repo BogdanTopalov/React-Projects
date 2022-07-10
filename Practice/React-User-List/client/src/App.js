98489cf293a3ef44ab0e5fc2b63b782e4ada0bae
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-10 22:52:27 +0300

    Add Search component

--- Practice/React-User-List/client/src/App.js
+++ Practice/React-User-List/client/src/App.js
@@ -2,12 +2,20 @@ import { Header } from "./components/common/Header";
 import { Footer } from "./components/common/Footer";
 
 import './App.css'
+import { Search } from "./components/search/Search";
 
 
 function App() {
     return (
         <div>
             <Header />
+
+            <main className='main'>
+                <section className="card users-container">
+                    <Search />
+                </section>
+            </main>
+
             <Footer />
         </div>
     );
