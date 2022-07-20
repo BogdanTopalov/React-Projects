7d9527e989ab37153365963978ba1900ef3b173d
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-20 16:00:17 +0300

    Move GET service to App level

--- Practice/Games-Play/games-play/src/App.js
+++ Practice/Games-Play/games-play/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Catalogue from './components/Catalogue/Catalogue';
@@ -6,8 +7,19 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import * as gameService from './services/gameService'
 
 function App() {
+    
+    const [games, setGames] = useState([])
+    
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                setGames(result)
+            })
+    }, [])
+
     return (
         <div id="box">
 
@@ -17,11 +29,11 @@ function App() {
             {/* Main Content */}
             <main id="main-content">
                 <Routes>
-                    <Route path="/" element={<Home />}/>
+                    <Route path="/" element={<Home games={games} />}/>
                     <Route path="/login" element={<Login />}/>
                     <Route path="/register" element={<Register />}/>
                     <Route path="/create" element={<CreateGame />}/>
-                    <Route path="/catalogue" element={<Catalogue />}/>
+                    <Route path="/catalogue" element={<Catalogue games={games} />}/>
                 </Routes>
             </main>
             
