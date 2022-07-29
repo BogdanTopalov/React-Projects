b3ed3eb6d17ca8243c2928d33a8a669da51f70da
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-29 13:38:16 +0300

    Add Logout functionality

--- Practice/Games-Play/games-play/src/App.js
+++ Practice/Games-Play/games-play/src/App.js
@@ -8,6 +8,7 @@ import GameDetails from './components/GameDetails/GameDetails';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Logout from './components/Logout/Logout';
 import Register from './components/Register/Register';
 
 import { AuthContext } from './context/AuthContext';
@@ -19,6 +20,10 @@ function App() {
 
     const userLogin = (authData) => {
         setAuth(authData)
+    }
+
+    const userLogout = () => {
+        setAuth({})
     }
     
     const addComment = (gameId, comment) => {
@@ -43,7 +48,7 @@ function App() {
     }, [])
 
     return (
-        <AuthContext.Provider value={{user: auth, userLogin}}>
+        <AuthContext.Provider value={{user: auth, userLogin, userLogout}}>
             <div id="box">
                 <Header />
 
@@ -52,6 +57,7 @@ function App() {
                         <Route path="/" element={<Home games={games} />}/>
                         <Route path="/login" element={<Login />}/>
                         <Route path="/register" element={<Register />}/>
+                        <Route path='/logout' element={<Logout />}/>
                         <Route path="/create" element={<CreateGame />}/>
                         <Route path="/catalogue" element={<Catalogue games={games} />}/>
                         <Route path="/catalogue/:gameId" element={<GameDetails games={games} addComment={addComment}/>} />
--- Practice/Games-Play/games-play/src/services/authService.js
+++ Practice/Games-Play/games-play/src/services/authService.js
@@ -1,7 +1,21 @@
 import { post } from "./requester"
 
-const baseUrl = 'http://localhost:3030'
+const baseUrl = 'http://localhost:3030/users'
 
 export const login = (email, password) => {
-    return post(`${baseUrl}/users/login`, { email, password })
+    return post(`${baseUrl}/login`, { email, password })
+}
+
+export const logout = async (accessToken) => {
+    try {
+        const response = await fetch(`${baseUrl}/logout`, {
+            headers: {
+                'X-Authorization': accessToken
+            }
+        })
+
+        return response
+    } catch (error) {
+        
+    }
 }
