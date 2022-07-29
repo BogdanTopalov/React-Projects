8b0519d7c257f5f100b5807d508891d52c987066
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-29 13:04:27 +0300

    Add AuthContext

--- Practice/Games-Play/games-play/src/App.js
+++ Practice/Games-Play/games-play/src/App.js
@@ -1,17 +1,39 @@
 import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Catalogue from './components/Catalogue/Catalogue';
 import CreateGame from './components/CreateGame/CreateGame';
+import GameDetails from './components/GameDetails/GameDetails';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+
+import { AuthContext } from './context/AuthContext';
 import * as gameService from './services/gameService'
 
 function App() {
-    
     const [games, setGames] = useState([])
+    const [auth, setAuth] = useState({})
+
+    const userLogin = (authData) => {
+        setAuth(authData)
+    }
+    
+    const addComment = (gameId, comment) => {
+        setGames(state => {
+            const game = state.find(x => x._id == gameId)
+
+            const comments = game.comments || []
+            comments.push(comment)
+
+            return [
+                ...state.filter(x => x._id !== gameId),
+                {...game, comments}
+            ]
+        })
+    }
     
     useEffect(() => {
         gameService.getAll()
@@ -21,42 +43,23 @@ function App() {
     }, [])
 
     return (
-        <div id="box">
+        <AuthContext.Provider value={{auth, userLogin}}>
+            <div id="box">
+                <Header />
 
-            {/* Navigation */}
-            <Header />
-
-            {/* Main Content */}
-            <main id="main-content">
-                <Routes>
-                    <Route path="/" element={<Home games={games} />}/>
-                    <Route path="/login" element={<Login />}/>
-                    <Route path="/register" element={<Register />}/>
-                    <Route path="/create" element={<CreateGame />}/>
-                    <Route path="/catalogue" element={<Catalogue games={games} />}/>
-                </Routes>
-            </main>
-            
-            {/*Home Page*/}
-
-            {/* Login Page ( Only for Guest users ) */}
-
-
-            {/* Register Page ( Only for Guest users ) */}
-
-
-            {/* Create Page ( Only for logged-in users ) */}
-
-
-            {/* Edit Page ( Only for the creator )*/}
-
-
-            {/*Details Page*/}
-
-
-            {/* Catalogue */}
-
-        </div>
+                <main id="main-content">
+                    <Routes>
+                        <Route path="/" element={<Home games={games} />}/>
+                        <Route path="/login" element={<Login />}/>
+                        <Route path="/register" element={<Register />}/>
+                        <Route path="/create" element={<CreateGame />}/>
+                        <Route path="/catalogue" element={<Catalogue games={games} />}/>
+                        <Route path="/catalogue/:gameId" element={<GameDetails games={games} addComment={addComment}/>} />
+                    </Routes>
+                </main>
+                
+            </div>
+        </AuthContext.Provider>
     );
 }
 
