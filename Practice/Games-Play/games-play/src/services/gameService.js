2452b530b5d670fcf43d755978dfc7a139c2a31a
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-29 12:19:01 +0300

    Add request functionality

--- Practice/Games-Play/games-play/src/services/gameService.js
+++ Practice/Games-Play/games-play/src/services/gameService.js
@@ -1,6 +1,8 @@
+import { request } from "./requester"
+
 const baseUrl = 'http://localhost:3030'
 
+
 export const getAll = () => {
-    return fetch(`${baseUrl}/data/games?sortBy=_createdOn%20desc&distinct=category`)
-        .then(res => res.json())
+    return request(`${baseUrl}/data/games?sortBy=_createdOn%20desc&distinct=category`)
 }
