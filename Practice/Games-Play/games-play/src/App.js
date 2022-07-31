5c2bec31e16c424fda3d8ff103bee6a833d377d7
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-31 17:04:37 +0300

    Optimize GameContext architecture

--- Practice/Games-Play/games-play/src/App.js
+++ Practice/Games-Play/games-play/src/App.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Catalogue from './components/Catalogue/Catalogue';
@@ -13,69 +12,29 @@ import Logout from './components/Logout/Logout';
 import Register from './components/Register/Register';
 
 import { AuthProvider } from './context/AuthContext';
-import { GameContext } from './context/GameContext';
-import * as gameService from './services/gameService'
+import { GameProvider } from './context/GameContext';
+
 
 function App() {
-    const [games, setGames] = useState([])
-    
-    const navigate = useNavigate()
-
-
-
-    const addGame = (gameData) => {
-        setGames(state => [
-            ...state,
-            gameData
-        ])
-
-        navigate('/catalogue')
-    }
-
-    const editGame = (gameId, gameData) => {
-        setGames(state => state.map(x => x._id === gameId ? gameData : x))
-    }
-
-    const addComment = (gameId, comment) => {
-        setGames(state => {
-            const game = state.find(x => x._id === gameId)
-
-            const comments = game.comments || []
-            comments.push(comment)
-
-            return [
-                ...state.filter(x => x._id !== gameId),
-                { ...game, comments }
-            ]
-        })
-    }
-
-    useEffect(() => {
-        gameService.getAll()
-            .then(result => {
-                setGames(result)
-            })
-    }, [])
-
     return (
         <AuthProvider>
             <div id="box">
                 <Header />
 
-                <GameContext.Provider value={{games, addGame, editGame}}>
+                <GameProvider>
                     <main id="main-content">
                         <Routes>
-                            <Route path="/" element={<Home games={games} />} />
+                            <Route path="/" element={<Home />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/register" element={<Register />} />
                             <Route path='/logout' element={<Logout />} />
                             <Route path="/create" element={<CreateGame />} />
                             <Route path="/games/:gameId/edit" element={<EditGame />} />
                             <Route path="/catalogue" element={<Catalogue />} />
-                            <Route path="/catalogue/:gameId" element={<GameDetails addComment={addComment} />} />
+                            <Route path="/catalogue/:gameId" element={<GameDetails />} />
                         </Routes>
                     </main>
-                </GameContext.Provider>
+                </GameProvider>
 
             </div>
         </AuthProvider>
--- Practice/Games-Play/games-play/src/context/GameContext.js
+++ Practice/Games-Play/games-play/src/context/GameContext.js
@@ -1,3 +1,79 @@
-import { createContext } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getAll } from "../services/gameService";
 
 export const GameContext = createContext()
+
+export const GameProvider = ({
+    children
+}) => {
+    const gameReducer = (state, action) => {
+        switch (action.type) {
+            case "ADD_GAMES":
+                return [...action.payload]
+            case "ADD_GAME":
+                return [...state, action.payload]
+            case "EDIT_GAME":
+                return state.map(x => x._id === action.gameId ? action.payload : x)
+            default:
+                return state
+        }
+    }
+
+    // const [games, setGames] = useState([])
+    const [games, dispatch] = useReducer(gameReducer, [])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        getAll()
+            .then(result => {
+                // setGames(result)
+                dispatch({
+                    type: 'ADD_GAMES',
+                    payload: result
+                })
+            })
+    }, [])
+
+    const addGame = (gameData) => {
+        // setGames(state => [
+        //     ...state,
+        //     gameData
+        // ])
+        dispatch({
+            type: 'ADD_GAME',
+            payload: gameData
+        })
+
+        navigate('/catalogue')
+    }
+
+    const editGame = (gameId, gameData) => {
+        // setGames(state => state.map(x => x._id === gameId ? gameData : x))
+        dispatch({
+            type: 'EDIT_GAME',
+            payload: gameData,
+            gameId
+        })
+    }
+
+    const addComment = (gameId, comment) => {
+        // setGames(state => {
+        //     const game = state.find(x => x._id === gameId)
+
+        //     const comments = game.comments || []
+        //     comments.push(comment)
+
+        //     return [
+        //         ...state.filter(x => x._id !== gameId),
+        //         { ...game, comments }
+        //     ]
+        // })
+    }
+
+    return (
+        <GameContext.Provider value={{games, addGame, editGame, addComment}}>
+            {children}
+        </GameContext.Provider>
+    )
+}
