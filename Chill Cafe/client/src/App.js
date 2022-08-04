a95053df28bfeec3fa0d09807e3233f6f71d08a7
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-04 14:10:17 +0300

    Update App.js

--- Chill Cafe/client/src/App.js
+++ Chill Cafe/client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Filters from './components/Filters/Filters';
+import Food from './components/Food/Food';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import SideNavigation from './components/SideNavigation/SideNavigation';
+import TopNavigation from './components/TopNavigation/TopNavigation';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<TopNavigation />
+			<SideNavigation />
+			<Filters />
+
+			<main>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/food' element={<Food />} />
+				</Routes>
+			</main>
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
