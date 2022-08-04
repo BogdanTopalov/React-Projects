ad64c9d6f5595b3455b42369e1d0c3ccc35bfa59
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-04 14:09:06 +0300

    Add SideNavigation component

---
+++ Chill Cafe/client/src/components/SideNavigation/SideNavigation.js
@@ -0,0 +1,15 @@
+const SideNavigation = () => {
+    return (
+        <section className="sidebar">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Food</a></li>
+                <li><a href="">Drinks</a></li>
+                <li><a href="">Reservation</a></li>
+            </ul>
+        </section>
+    )
+}
+
+export default SideNavigation
