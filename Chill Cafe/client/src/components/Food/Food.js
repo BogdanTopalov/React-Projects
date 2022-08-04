546ff660e9500f711659104276c3465c309dc809
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-04 14:08:39 +0300

    Add Food component

---
+++ Chill Cafe/client/src/components/Food/Food.js
@@ -0,0 +1,57 @@
+const Food = () => {
+    return (
+        <div className="catalogue">
+            <a className="item" href="">
+                <div className="item-details">
+                    <h3>Coffee</h3>
+                    <p>3$</p>
+                </div>
+            </a>
+            <a className="item" href="">
+                <div className="item-details">
+                    <h3>Coffee</h3>
+                    <p>3$</p>
+                </div>
+            </a>
+            <a className="item" href="">
+                <div className="item-details">
+                    <h3>Coffee</h3>
+                    <p>3$</p>
+                </div>
+            </a>
+            <a className="item" href="">
+                <div className="item-details">
+                    <h3>Coffee</h3>
+                    <p>3$</p>
+                </div>
+            </a>
+            <a className="item" href="">
+                <div className="item-details">
+                    <h3>Coffee</h3>
+                    <p>3$</p>
+                </div>
+            </a>
+            <a className="item" href="">
+                <div className="item-details">
+                    <h3>Coffee</h3>
+                    <p>3$</p>
+                </div>
+            </a>
+            <a className="item" href="">
+                <div className="item-details">
+                    <h3>Coffee</h3>
+                    <p>3$</p>
+                </div>
+            </a>
+            <a className="item" href="">
+                <div className="item-details">
+                    <h3>Coffee</h3>
+                    <p>3$</p>
+                </div>
+            </a>
+
+        </div>
+    )
+}
+
+export default Food
