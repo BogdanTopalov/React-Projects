2b811343960f2efdfc51f802a73531eb5c93c05e
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-07 22:50:01 +0300

    Update App.js

--- Chill Cafe/client/src/App.js
+++ Chill Cafe/client/src/App.js
@@ -1,16 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Drinks from './components/Drinks/Drinks';
 import Filters from './components/Filters/Filters';
 import Food from './components/Food/Food';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Logout from './components/Logout/Logout';
+import Register from './components/Register/Register';
 import SideNavigation from './components/SideNavigation/SideNavigation';
 import TopNavigation from './components/TopNavigation/TopNavigation';
+import { AuthProvider } from './context/AuthContext';
 
 
 function App() {
 	return (
-		<>
+		<AuthProvider>
 			<TopNavigation />
 			<SideNavigation />
 			<Filters />
@@ -19,11 +24,15 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/food' element={<Food />} />
+					<Route path='/drinks' element={<Drinks />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/logout' element={<Logout />} />
 				</Routes>
 			</main>
 
 			<Footer />
-		</>
+		</AuthProvider>
 	);
 }
 
