7c4f2f320f133bd2a9779cd6cbede14c57406d09
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-08 23:24:41 +0300

    Create Reservation.js

---
+++ Chill Cafe/client/src/components/Reservation/Reservation.js
@@ -0,0 +1,8 @@
+const Reservation = () => {
+    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1550293750-dde2bed30d54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
+    return (
+        NaN
+    )
+}
+
+export default Reservation
