9256049b0ad3a165a49cc2655e11a10d976cc5db
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-08 23:24:18 +0300

    Update Drinks.js

--- Chill Cafe/client/src/components/Drinks/Drinks.js
+++ Chill Cafe/client/src/components/Drinks/Drinks.js
@@ -1,4 +1,9 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandPointRight } from '@fortawesome/free-regular-svg-icons'
+
+import { MainContext } from "../../context/MainContext"
 import { getAllDrinks } from "../../services/drinksService"
 import DrinksItem from "./DrinksItem"
 
@@ -6,18 +11,21 @@ import DrinksItem from "./DrinksItem"
 const Drinks = () => {
     const [drinks, setDrinks] = useState([])
 
+    const { selectedDrink } = useContext(MainContext)
+
     useEffect(() => {
         getAllDrinks()
             .then(result => {
-                setDrinks(Object.values(result))
+                const resultList = Object.values(result).filter(x => x.type === selectedDrink)
+                setDrinks(resultList)
             })
-    }, [])
+    }, [selectedDrink])
 
     return (
         <div className="catalogue">
             {drinks.length > 0
                 ? drinks.map(d => <DrinksItem key={d._id} item={d} />)
-                : <h2>No drinks :/ </h2>
+                : <h2>Select a drink type from here <FontAwesomeIcon icon={faHandPointRight}/></h2>
             }
         </div>
     )
