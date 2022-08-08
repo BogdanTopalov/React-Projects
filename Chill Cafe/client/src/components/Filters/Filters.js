9284ce6b5929edeb0c5e280aa552e6c3228be23f
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-08 23:23:46 +0300

    Update Filters.js

--- Chill Cafe/client/src/components/Filters/Filters.js
+++ Chill Cafe/client/src/components/Filters/Filters.js
@@ -1,20 +1,42 @@
+import { useContext } from "react"
 import { useLocation } from "react-router-dom"
+import { MainContext } from "../../context/MainContext"
 
 const Filters = () => {
     const location = useLocation()
-    if (location.pathname === '/food' || location.pathname === '/drinks') {
+
+    const { setSelectedFood } = useContext(MainContext)
+    const { setSelectedDrink } = useContext(MainContext)
+
+    const onDrinkClickHandler = (e) => {
+        e.preventDefault()
+        setSelectedDrink(e.target.value)
+    }
+
+    const onFoodClickHandler = (e) => {
+        e.preventDefault()
+        setSelectedFood(e.target.value)
+    }
+
+    if (location.pathname === '/food') {
         return (
             <section className="filters">
                 <ul>
-                    <li><a href="">Breakfast</a></li>
-                    <li><a href="">Alcoholic</a></li>
-                    <li><a href="">Alabala</a></li>
-                    <li><a href="">Brunch</a></li>
-                    <li><a href="">Natural</a></li>
+                    <button onClick={onFoodClickHandler} value='Fast Food'>Fast Food</button>
+                    <button onClick={onFoodClickHandler} value='Dessert'>Dessert</button>
                 </ul>
             </section>
         )
-    }
+    } else if (location.pathname === '/drinks') {
+            return (
+                <section className="filters">
+                    <ul>
+                        <button onClick={onDrinkClickHandler} value='Coffee'>Coffee</button>
+                        <button onClick={onDrinkClickHandler} value='Cocktail'>Cocktails</button>
+                    </ul>
+                </section>
+            )
+        }
 }
 
 export default Filters
