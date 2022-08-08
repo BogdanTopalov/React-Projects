c7e4750be734930dbbead06ceb00144e8f9845f1
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-08 23:24:15 +0300

    Update Food.js

--- Chill Cafe/client/src/components/Food/Food.js
+++ Chill Cafe/client/src/components/Food/Food.js
@@ -1,4 +1,9 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandPointRight } from '@fortawesome/free-regular-svg-icons'
+
+import { MainContext } from "../../context/MainContext"
 import { getAllFood } from "../../services/foodService"
 import FoodItem from "./FoodItem"
 
@@ -6,18 +11,21 @@ import FoodItem from "./FoodItem"
 const Food = () => {
     const [food, setFood] = useState([])
 
+    const { selectedFood } = useContext(MainContext)
+
     useEffect(() => {
         getAllFood()
             .then(result => {
-                setFood(Object.values(result))
+                const resultList = Object.values(result).filter(x => x.type === selectedFood)
+                setFood(resultList)
             })
-    }, [])
+    }, [selectedFood])
 
     return (
         <div className="catalogue">
             {food.length > 0
                 ? food.map(f => <FoodItem key={f._id} item={f} />)
-                : <h2>No food :/ </h2>
+                : <h2>Select a food type from here <FontAwesomeIcon icon={faHandPointRight}/></h2>
             }
         </div>
     )
