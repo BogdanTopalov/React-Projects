6d7325d1fee672f5e111019078d5e869f9f29d0a
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-09 22:35:50 +0300

    Update FoodItemDetails.js

--- Chill Cafe/client/src/components/Food/FoodItemDetails.js
+++ Chill Cafe/client/src/components/Food/FoodItemDetails.js
@@ -1,6 +1,40 @@
+import { useContext, useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { AuthContext } from "../../context/AuthContext"
+import { getOneFood } from "../../services/foodService"
+
+
 const FoodItemDetails = () => {
+    let { foodId } = useParams()
+    const [currentFood, setCurrentFood] = useState({})
+    const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+        getOneFood(foodId)
+            .then(result => {
+                setCurrentFood(result)
+            })
+    }, [foodId])
+
     return (
-        NaN
+        <div className="details">
+            <img src={currentFood.imageUrl} alt='img' />
+            <div className="info">
+                <div>
+                    <h1>{currentFood.name}</h1>
+                    <h2>Price: &euro;{currentFood.price}</h2>
+                    <h3>Type: {currentFood.type}</h3>
+                </div>
+
+                {user.email === 'admin@abv.bg'
+                    ? <div className="buttons">
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                    : <></>
+                }
+            </div>
+        </div>
     )
 }
 
