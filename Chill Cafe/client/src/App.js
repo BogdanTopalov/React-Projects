d303edfe70ea7a0b2901edde146e1b08fb628d03
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-09 22:37:14 +0300

    Update App.js

--- Chill Cafe/client/src/App.js
+++ Chill Cafe/client/src/App.js
@@ -1,12 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddItem from './components/AddItem/AddItem';
 import Drinks from './components/Drinks/Drinks';
+import DrinksItemDetails from './components/Drinks/DrinksItemDetails';
+import EditDrink from './components/Drinks/EditDrink';
 import Filters from './components/Filters/Filters';
 import Food from './components/Food/Food';
+import FoodItemDetails from './components/Food/FoodItemDetails';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Logout from './components/Logout/Logout';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Profile from './components/Profile/Profile';
 import Register from './components/Register/Register';
 import Reservation from './components/Reservation/Reservation';
@@ -27,8 +32,14 @@ function App() {
 				<main>
 					<Routes>
 						<Route path='/' element={<Home />} />
+						<Route element={<PrivateRoute />} >
+							<Route path='/add' element={<AddItem />} />
+							<Route path='/drinks/:drinkId/edit' element={<EditDrink />} />
+						</Route>
 						<Route path='/food' element={<Food />} />
+						<Route path='/food/:foodId' element={<FoodItemDetails />} />
 						<Route path='/drinks' element={<Drinks />} />
+						<Route path='/drinks/:drinkId' element={<DrinksItemDetails />} />
 						<Route path='/login' element={<Login />} />
 						<Route path='/register' element={<Register />} />
 						<Route path='/logout' element={<Logout />} />
