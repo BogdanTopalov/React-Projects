a93b6e610256a6f2f80072c5fc25261ca172c2a3
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-09 22:36:11 +0300

    Update DrinksItemDetails.js

--- Chill Cafe/client/src/components/Drinks/DrinksItemDetails.js
+++ Chill Cafe/client/src/components/Drinks/DrinksItemDetails.js
@@ -1,6 +1,54 @@
+import { useContext, useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { AuthContext } from "../../context/AuthContext"
+import { getOneDrink, removeDrink } from "../../services/drinksService"
+
+
 const DrinksItemDetails = () => {
+    let { drinkId } = useParams()
+    const [currentDrink, setCurrentDrink] = useState({})
+    const { user } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        getOneDrink(drinkId)
+            .then(result => {
+                setCurrentDrink(result)
+            })
+    }, [drinkId])
+
+    const onEditHandler = () => {
+        navigate(`/drinks/${drinkId}/edit`)
+    }
+
+    const onDeleteHandler = () => {
+        const confirm = window.confirm('Do you want to delete this drink?')
+
+        if (confirm) {
+            removeDrink(drinkId)
+            navigate('/drinks')
+        }
+    }
+
     return (
-        NaN
+        <div className="details">
+            <img src={currentDrink.imageUrl} alt='img' />
+            <div className="info">
+                <div>
+                    <h1>{currentDrink.name}</h1>
+                    <h2>Price: &euro;{currentDrink.price}</h2>
+                    <h3>Type: {currentDrink.type}</h3>
+                </div>
+
+                {user.email === 'admin@abv.bg'
+                    ? <div className="buttons">
+                        <button onClick={onEditHandler}>Edit</button>
+                        <button onClick={onDeleteHandler}>Delete</button>
+                    </div>
+                    : <></>
+                }
+            </div>
+        </div>
     )
 }
 
