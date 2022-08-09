0a34792fae1afbbc3c2c978cb007c4be2e1b226b
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-09 22:36:18 +0300

    Update Drinks.js

--- Chill Cafe/client/src/components/Drinks/Drinks.js
+++ Chill Cafe/client/src/components/Drinks/Drinks.js
@@ -9,6 +9,8 @@ import DrinksItem from "./DrinksItem"
 
 
 const Drinks = () => {
+    document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1530064161350-7824b8cdeee9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")'
+    
     const [drinks, setDrinks] = useState([])
 
     const { selectedDrink } = useContext(MainContext)
@@ -25,7 +27,10 @@ const Drinks = () => {
         <div className="catalogue">
             {drinks.length > 0
                 ? drinks.map(d => <DrinksItem key={d._id} item={d} />)
-                : <h2>Select a drink type from here <FontAwesomeIcon icon={faHandPointRight}/></h2>
+                : !selectedDrink
+                    ? <h2>Select a drink type from here <FontAwesomeIcon icon={faHandPointRight}/></h2>
+                    : <h2>No {selectedDrink}s at the moment, select another <FontAwesomeIcon icon={faHandPointRight}/></h2>
+                    
             }
         </div>
     )
